436ba67f6b101b2526ccdee61c2efd5c781fb58e
TamaraZolotovskaya <tamarafx25@gmail.com>
2023-01-24 16:11:38 +0700

    Веб-приложение для автоматизации учета товаров на складе носков

---
+++ src/main/java/me/tamarazolotovskaya/simplesosks3/SimpleSosks3Application.java
@@ -0,0 +1,13 @@
+package me.tamarazolotovskaya.simplesosks3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSosks3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSosks3Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/tamarazolotovskaya/simplesosks3/SimpleSosks3ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tamarazolotovskaya.simplesosks3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSosks3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
